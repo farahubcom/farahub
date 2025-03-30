283ada98952cf491b695c23820c1080737cfcd1b
Farzin Kakh <farzinask1996@gmail.com>
2025-03-30 20:56:28 +0330

    apps directory structure changed

--- apps/hub/controllers/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res) {
-    return res.render("index", {
-        layout: false,
-        shared: {
-            hostname: req.hostname.replace("my.", "")
-        }
-    });
-}
