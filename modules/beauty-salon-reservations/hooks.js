8b282686ef0a715f3fc2c562221131de46069928
Farzin Kakh <farzinask1996@gmail.com>
2025-04-11 20:26:01 +0330

    beauty-salon-reservations settings hooks added

--- modules/beauty-salon-reservations/hooks.js
+++ modules/beauty-salon-reservations/hooks.js
@@ -1,4 +1,28 @@
+const pick = require("lodash/pick");
+const setWith = require('lodash/setWith');
+const clone = require('lodash/clone');
+
 const hooks = module => ({
+    'Core': {
+        'workspace.settings.preSave': async ({ workspace, data }) => {
+            
+            const modifying = pick(data, [
+                'beauty-salon-reservations:pricing:default',
+                'beauty-salon-reservations:online-booking:name', 
+                'beauty-salon-reservations:online-booking:headline', 
+                'beauty-salon-reservations:online-booking:about', 
+                'beauty-salon-reservations:online-booking:tel',
+                'beauty-salon-reservations:online-booking:website',
+                'beauty-salon-reservations:online-booking:instagram',
+                'beauty-salon-reservations:online-booking:address',
+                'beauty-salon-reservations:online-booking:latlng'
+            ]);
+
+            Object.keys(modifying).forEach((key) => {
+                workspace.options = setWith(clone(workspace.options), key, data[key], clone)
+            });
+        },
+    },
     'Products': {
         'main.createOrUpdate.validate': async () => {
             return {
